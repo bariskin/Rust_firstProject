9c86fb352d78aa7a00c4b2bba737fd1b04ffaa8c
kai <kai@mtl.ru>
2022-05-12 17:40:52 +0300

    added break for exit

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,26 +12,28 @@ println!("Загаданное число: {}", secret_number);
 
 loop {
 
-println!("Пожалуйста, введите предположение.");
+  println!("Пожалуйста, введите предположение.");
 
-let mut guess = String::new();
+  let mut guess = String::new();
 
-io::stdin()
-.read_line(&mut guess)
-.ok()
-.expect("Не удалось прочитать строку");     // io::stdin().read_line(&mut guess).ok().expect("Не удалось прочитать строку");
+   io::stdin()
+   .read_line(&mut guess)
+   .ok()
+   .expect("Не удалось прочитать строку");     // io::stdin().read_line(&mut guess).ok().expect("Не удалось прочитать строку");
 
-let guess: u32 = guess.trim().parse()       // string convert to int
-.ok()
-.expect("Пожалуйста, введите число!");
+   let guess: u32 = guess.trim().parse()       // string convert to int
+   .ok()
+   .expect("Пожалуйста, введите число!");
 
+   println!("Ваша попытка: {}", guess);
 
-println!("Ваша попытка: {}", guess);
-
-match guess.cmp(&secret_number) {            // сравнение чисел
-    Ordering::Less => println!("Слишком маленькое!"),
-    Ordering::Greater => println!("Слишком большое!"),
-    Ordering::Equal => println!("Вы выиграли!"),
+   match guess.cmp(&secret_number) {            // сравнение чисел
+      Ordering::Less => println!("Слишком маленькое!"),
+      Ordering::Greater => println!("Слишком большое!"),
+      Ordering::Equal => {
+                          println!("Вы выиграли!");
+                          break;
     }
+  }
  }
 }
