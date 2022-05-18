96c854df938ca7f54e75f2f695b7a433db111d17
kai <kai@mtl.ru>
2022-05-18 16:08:29 +0300

    added project phislosoph_eat

---
+++ philosopher_eat/target/release/deps/philosopher_eat.d
@@ -0,0 +1,5 @@
+D:\RUST_уроки\philosopher_eat\target\release\deps\philosopher_eat.exe: src\main.rs
+
+D:\RUST_уроки\philosopher_eat\target\release\deps\philosopher_eat.d: src\main.rs
+
+src\main.rs:
---
+++ philosopher_eat/target/release/philosopher_eat.d
@@ -0,0 +1,1 @@
+D:\RUST_уроки\philosopher_eat\target\release\philosopher_eat.exe: D:\RUST_уроки\philosopher_eat\src\main.rs
