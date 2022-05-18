f3716239fb387b40253fa636b0abfa105c4d2c88
kai <kai@mtl.ru>
2022-05-18 17:01:26 +0300

    added: 1) пример вывода на печать, 2) пример запуска метода экземпляра

--- philosopher_eat/src/main.rs
+++ philosopher_eat/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::thread;
+
+struct Philosopher {
+       name: String,
 }
+
+    impl Philosopher {
+
+        fn new(name: &str) -> Philosopher {
+           Philosopher {
+           name: name.to_string(),
+          }
+       }
+
+       fn eat(&self){
+           println!("{} начала есть.", self.name);                    /* пример вывода на печать */
+
+           thread::sleep_ms(1000);
+
+           println!("{} закончил есть.\n", self.name);
+       }
+   }
+ 
+fn main() {
+
+    let philosophers = vec![                                    /* пример создание вектора */
+            Philosopher::new("Джудит Батлер"),
+            Philosopher::new("Рая Дунаевская"),
+            Philosopher::new("Зарубина Наталья"),
+            Philosopher::new("Эмма Гольдман"),
+            Philosopher::new("Анна Шмидт"),
+          ];     
+
+        for p in &philosophers {
+              p.eat();
+         }    
+    }
---
+++ philosopher_eat/target/debug/philosopher_eat.d
@@ -0,0 +1,1 @@
+D:\RUST_уроки\philosopher_eat\target\debug\philosopher_eat.exe: D:\RUST_уроки\philosopher_eat\src\main.rs
