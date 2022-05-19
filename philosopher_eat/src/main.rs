6bc096630af3d45aefbf4ba26e68b4402952b60f
kai <kai@mtl.ru>
2022-05-19 17:46:42 +0300

    added mutex for threads

--- philosopher_eat/src/main.rs
+++ philosopher_eat/src/main.rs
@@ -1,26 +1,42 @@
 
 use std::thread;
+use std::sync::{Mutex, Arc};
+
 
 struct Philosopher {
        name: String,
+       //left: usize,
+       //right:usize,
 }
 
-    impl Philosopher {
-
-        fn new(name: &str) -> Philosopher {
-           Philosopher {
+impl Philosopher {
+    //fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        fn new(name: &str) -> Philosopher {  
+          Philosopher {
            name: name.to_string(),
-          }
-       }
+          //  left: left,
+          // right: right,
+         }
 
-       fn eat(&self){
-           println!("{} начала есть.", self.name);                    /* пример вывода на печать */
+     }
 
-           thread::sleep_ms(1000);
+    fn eat(&self){
+    
+      //  let _left = table.forks[self.left].lock().unwrap();
+       // let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} начала есть.", self.name);                    /* пример вывода на печать */
+
+        thread::sleep_ms(3000);
 
            println!("{} закончил есть.\n", self.name);
        }
    }
+
+
+struct Table {
+     forks: Vec<Mutex<()>>,
+}
  
 fn main() {
 
@@ -31,6 +47,16 @@ fn main() {
             Philosopher::new("Эмма Гольдман"),
             Philosopher::new("Анна Шмидт"),
           ];     
+
+
+      /* 
+         Объявляем новое связанное имя handles . Мы задали такое имя, потому что собираемся
+         создать несколько потоков, в результате чего получим для них дескрипторы, с помощью
+         которых сможем контролировать их выполнение. Здесь нам нужно явно указать тип, а зачем
+         это необходимо, мы расскажем чуть позже. _ - это заполнитель типа. Мы говорим
+         
+         компилятору:« handles — это вектор, содержащий элементы, тип которых Rust должен вывести самостоятельно».
+      */
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
             thread::spawn(move || {
